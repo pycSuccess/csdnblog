eae8a7bbcbff1ad83320a3aaf65a5629a30001d3
pycSuccess <3296389840@qq.com>
2019-07-01 18:31:47 +0800

    没写什么东西也得提交一波

--- blog/blog_urls.py
+++ blog/blog_urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     url(r'^index/', views.index, name='index'),
     url(r'^login/', views.login, name='login'),
     url(r'^logout/', views.logout, name='logout'),
+    url(r'^home_work/', views.home_work, name='home_work'),
     url(r'(?P<username>\w+)', views.home_site),
     url(r'.', views.index),
 
--- blog/views.py
+++ blog/views.py
@@ -1,16 +1,18 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, HttpResponse
 from django.contrib import auth
 from django.urls import reverse
+from django.db.models import Count
 # Create your views here.
 
 
 from blog.models import Article
 from blog.models import UserInfo
+from blog.models import Category
+from blog.models import Tag
 
 
 def index(request):
     article_list = Article.objects.all()
-    print(article_list)
     return render(request, 'index.html', {'article_list': article_list})
 
 
@@ -36,5 +38,31 @@ def home_site(request, username):
     user = UserInfo.objects.filter(username=username).first()
     if not user:
         return render(request, 'not_found.html')
-    article_lists = Article.objects.filter(user__username=username)
-    return render(request, 'home_site.html', {'user': user, 'article_list': article_lists})
+    article_list = Article.objects.filter(user__username=username)
+    blog = user.blog
+    cate_list = Category.objects.filter(blog=blog).annotate(c=Count('article__nid')).values('title', 'c')
+    tag_list = Tag.objects.filter(blog=blog).annotate(c=Count('article__nid')).values('title', 'c')
+    dict = {
+        "blog": blog,
+        "article_list": article_list,
+        "category_count": cate_list,
+        "tag_count": tag_list,
+    }
+    print(cate_list, type(cate_list))
+    return render(request, 'home_work.html', dict)
+
+
+# 查询当前站点每一个分类的名称以及对应的文章数
+# 查询当前站点每一个标签的名称以及对应的文章数
+def home_work(request):
+    # articles = Article.objects.all().annotate(c=Count('nid')).values('category__title', 'c').distinct()
+    # articles = Category.objects.all().annotate(c=Count('article__nid')).values('title', 'c')
+    # print(articles)
+    # articles = Tag.objects.all().annotate(c=Count('article__nid')).values('title', 'c')
+    # print(articles)
+    # test_date = Article.objects.filter(comment_count=0).extra(select={'y_m_date': 'create_time' > '2017-09-05'})
+    # print(test_date)
+    date_list = Article.objects.all().extra(select={"y_m_date": "date_format('%%Y/%%m',create_time)"}).values(
+        "y_m_date")
+    print(date_list[0].get('y_m_date'))
+    return HttpResponse('ok')
